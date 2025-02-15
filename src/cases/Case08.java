fcaebe6d7ae62b80744faba256bb15dfcf1f82c7
Umarbekdeveloper <umarbekbahromov07@gmail.com>
2025-02-15 23:00:44 +0500

    cases added

--- src/cases/Case08.java
+++ src/cases/Case08.java
@@ -4,19 +4,26 @@ import java.util.Scanner;
 
 public class Case08 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number 1<=k<=4: ");
-        int amal = sc.nextInt();
-        System.out.print("D: ");
-        int d = sc.nextInt();
-        System.out.print("M: ");
-        int m = sc.nextInt();
-//        switch (amal){
-//            case 1 -> System.out.println("a+b = "+(A+B));
-//            case 2 -> System.out.println("a-b = "+(A-B));
-//            case 3 -> System.out.println("a/b = "+(A/B));
-//            case 4 -> System.out.println("a*b = "+(A*B));
-//            default -> System.out.println("Amal xato!!");
-//        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kabisa bo'lmagan yil oy sanasi kiritilsin!!");
+        System.out.print("Kunni kiriting (1-31): ");
+        int D = scanner.nextInt();
+        System.out.print("Oyni kiriting (1-12): ");
+        int M = scanner.nextInt();
+        switch (M) {
+            case 1 -> System.out.println(D+" - "+"yanvar");
+            case 2 ->System.out.println(D+" - "+"fevral");
+            case 3 ->System.out.println(D+" - "+"mart");
+            case 4 ->System.out.println(D+" - "+"aprel");
+            case 5 ->System.out.println(D+" - "+"may");
+            case 6 ->System.out.println(D+" - "+"iyun");
+            case 7 ->System.out.println(D+" - "+"iyul");
+            case 8 ->System.out.println(D+" - "+"avgust");
+            case 9 ->System.out.println(D+" - "+"sentyabr");
+            case 10 ->System.out.println(D+" - "+"oktyabr");
+            case 11 ->System.out.println(D+" - "+"noyabr");
+            case 12 ->System.out.println(D+" - "+"dekabr");
+            default -> System.out.println("Noto'g'ri oy kiritildi!");
+        }
     }
 }
